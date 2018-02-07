6126c727d63b5cd262db243584237c2bb439086f
xiaoqi.huxq <xiaoqi.huxq@alibaba-inc.com>
2018-02-07 15:13:25 +0800

    chore: add bezier easing raf entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
-var webpack = require('webpack');
-
-// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 module.exports = {
-  //插件项
   plugins: [],
-  //页面入口文件配置
   entry: {
-    index: './src/index.js'
+    index: './src/index.js',
+    easing: './src/easing.js',
+    raf: './src/raf.js',
+    bezier:'./src/bezier.js'
   },
-  //入口文件输出配置
   output: {
     path: __dirname + '/lib/',
     filename: '[name].js',
     libraryTarget:'umd'
-  },
-  devServer: {
-    contentBase: "./demo",  //以public为根目录提供文件
-    historyApiFallback: true,
-    inline: true,
-    publicPath: '/dist/'
   },
   module: {
     //加载器配置
@@ -27,10 +18,6 @@ module.exports = {
         test: /\.css$/,
         loader: 'style-loader!css-loader'
       },
-      // {
-      //   test: /\.js$/,
-      //   loader: 'jsx-loader?harmony'
-      // },
       {
         test: /\.(png|jpg)$/,
         loader: 'url-loader?limit=8192'
